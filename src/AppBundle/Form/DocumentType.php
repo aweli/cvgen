6a2ad195d1bda8f78e2180e10c52f0cb31533e66
alexwerffeli <58297365et>
2016-10-17 20:35:28 +0200

    Refactor Entity attributes - set fields to not mandatory - create default page

--- src/AppBundle/Form/DocumentType.php
+++ src/AppBundle/Form/DocumentType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,9 @@ class DocumentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('company')
+            ->add('company', TextType::class, [
+                'required' => false,
+            ])
             ->add('title')
             ->add('subTitle')
             ->add('intro')
